cc40cde4e1e85007de9d1804391d1729ab436c0a
Jess <121574569+Jzoll@users.noreply.github.com>
2023-01-19 14:24:46 -0600

    Add files via upload
    Homework 1 initial entry

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+public static void Main(string[] args)
+    {
+        float X = 2.5f;
+        float Y = 3.3f;
+        float Z = 4*X*X+3*Y;
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.Write("The value of Z is: ");
+        Console.WriteLine(Z);
+    }
+}
