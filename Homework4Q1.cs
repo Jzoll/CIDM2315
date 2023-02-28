607f962c8ee31702683b10db01069f92972c9809
Jess <121574569+Jzoll@users.noreply.github.com>
2023-02-28 11:52:06 -0600

    Add files via upload
    Homework 4 program files

---
+++ Homework4Q1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+//Main method takes two user input and sets it as int x and int y
+static void Main(string[] args)
+{
+    Console.WriteLine("Enter the first number:");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number:");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("a = " + x);
+    Console.WriteLine("; b = " + y);
+    //calls compare method and applies to user defined integers
+    Compare(x,y);
+}
+static void Compare(int x, int y)
+{
+    //compares x & y and prints largest value
+    if(x<y)
+    {
+        Console.WriteLine("The largest number is: " + y);
+    }
+    else
+    {
+        Console.WriteLine("The largest number is: " + x);
+    }
+}
+}
---
+++ Homework4Q2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the left or right to define the shape:");
+        string shape = Console.ReadLine().ToLower();
+
+        Console.Write("N is : " + x);
+        Console.WriteLine("; shape is " + shape);
+    
+        Option(x, shape);
+    }
+    static void Option(int x, string shape) 
+    {
+        //check "shape" and run Left or Right
+        if(shape == "left"){
+        Left(x, shape);
+        }
+        else
+        {
+        //if program cannot parse input for "left" default case is to run Right
+        Right(x, shape);
+        }
+    }
+    static void Left(int x, string shape) 
+    {
+        //Left triangle
+        for(int i = 0; i < x; i++)
+        {
+            //Inner Loop prints * and " "
+            for(int p = 0; p < x; p++)
+            {
+                if (p<=i)
+                {
+                Console.Write("*");
+                }
+                else
+                {
+                Console.Write(" ");
+                }
+        
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Right(int x, string shape) 
+    {
+        int m = x;
+        int i = 1;
+        //numberOfSpaces is countdown from num to keep track of " "
+        int numberOfSpaces = m-i;
+
+        //Outer loop is a counter and stops when i is greater than num. loop also prints new line(tab)
+        for(i = 1; i <= x; i++)
+        {
+            //Inner loop prints spaces and then numbers according to k
+            for(int p = 1; p <= x; p++)
+            {
+                //prints spaces
+                if (p<=numberOfSpaces)
+                {
+                Console.Write(" ");
+                }
+                //prints number
+                else{
+                Console.Write("*");
+                }
+            }
+            numberOfSpaces--;
+            //this write line is how this is counting new lines
+            Console.WriteLine();
+        }
+    }
+}
